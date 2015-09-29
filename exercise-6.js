eafa252426a7be71f8a955c566586459b576e3b0
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-29 19:40:04 +0000

    Exercise 6  is hopefuly working

--- exercise-6.js
+++ exercise-6.js
@@ -4,7 +4,6 @@ Promise.promisifyAll(mysql);
 Promise.promisifyAll(require("mysql/lib/Connection").prototype);
 Promise.promisifyAll(require("mysql/lib/Pool").prototype);
 var colors = require('colors');
-
 var connection = mysql.createConnection({
   host     : process.env.IP,
   user     : process.env.C9_USER,
@@ -13,52 +12,51 @@ var connection = mysql.createConnection({
 });
 
 connection.queryAsync("SELECT * FROM Account LIMIT 10;").then(
-    function(results) {
-        
-    	return results[0];
-    }
-).map( function(arrayAccounts){
+    function(result) {
+        var rows = result[0];
+        var idSelected = rows.map(function(x){ return x.id }).join(", ");
+    	return connection.queryAsync("SELECT * FROM AddressBook WHERE accountId IN ("+idSelected+");").then( 
+    	    
+    	    function(results){
     
-    return {id: arrayAccounts.id, email: arrayAccounts.email};
-
-}).map( function(formatArray){
-    
-    return connection.queryAsync("SELECT * FROM AddressBook WHERE accountId = "+formatArray.id+";").then(
-        
-        function(custAddressbooks){
-            
-            var rowsAddressbook = custAddressbooks[0].map(function(addressbookRow) {
+                var accounts = {};
                 
-                return addressbookRow.name;
-            });
-
-            return {id: formatArray.id, email: formatArray.email, addressbooks: rowsAddressbook};
-        }
-    );    
-}).then( function(formatArray){
-    
-    for(var j = 0 ; j < formatArray.length ; j++){
-        
-        if(formatArray[j].id < 10){
-        
-            console.log(("\n  #"+formatArray[j].id+": ") .bold +formatArray[j].email+"\n");
-            console.log("\tAddress Books:" .bgBlue.white.bold);
-        } else if (formatArray[j].id < 100){
+                for (var i = 0; i < results[0].length; i++) {
+                    
+                    var entries = results[0];
+                    var entry = entries[i];
+                    var accountId = entry.accountId;
+                    
+                    if(!accounts[accountId]){
+                        
+                        accounts[accountId] = {id: accountId, email: entry.email, addressBooks: []};
+                    }
+                    
+                    accounts[accountId].addressBooks.push(entry.name);
+                }    
+                var keysFormatArray = Object.keys(accounts);
             
-            console.log(("\n #"+formatArray[j].id+": ") .bold +formatArray[j].email+"\n");
-            console.log("\tAddress Books:" .bgBlue.white.bold);
-        } else {
-            
-            console.log(("\n#"+formatArray[j].id+": ") .bold +formatArray[j].email+"\n");
-            console.log("\tAddress Books:" .bgBlue.white.bold);
-        }
-        
-        for(var i = 0 ; i < formatArray[j].addressbooks.length ; i++){
-            
-            console.log("\t"+formatArray[j].addressbooks[i] .rainbow);
-        }
+                for(var i = 0 ; i < keysFormatArray.length ; i++){
+                    
+                    if(keysFormatArray[i] < 10){
+                        console.log(("\n  #"+accounts[keysFormatArray[i]].id+": ") .bold +result[0][i].email+"\n");
+                        console.log("\tAddress Books:" .bgBlue.white.bold);
+                    } else if (keysFormatArray[i] < 100){
+                        console.log(("\n #"+accounts[keysFormatArray[i]].id+": ") .bold +result[0][i].email+"\n");
+                        console.log("\tAddress Books:" .bgBlue.white.bold);
+                    } else {
+                        console.log(("\n#"+accounts[keysFormatArray[i]].id+": ") .bold +result[0][i].email+"\n");
+                        console.log("\tAddress Books:" .bgBlue.white.bold);
+                    }
+                    
+                    for(var j = 0 ; j < accounts[keysFormatArray[i]].addressBooks.length ; j++){
+                        console.log("\t"+accounts[keysFormatArray[i]].addressBooks[j] .rainbow); 
+                    }
+                }
+            }
+        );
     }
-}).finally(
+).finally(
     function() {
         connection.end();
     }
